abf16fd5a86d1fc395f518537659f75b790c43f9
Kunal Bansal <git+qnlbnsl@gmail.com>
2022-08-26 15:02:25 -0400

    template maintenance

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,24 @@
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import serve from 'rollup-plugin-serve';
+
+import postcss from 'rollup-plugin-postcss';
 import json from '@rollup/plugin-json';
 import ignore from './rollup-plugins/ignore';
+
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+
 import { ignoreTextfieldFiles } from './elements/ignore/textfield';
 import { ignoreSelectFiles } from './elements/ignore/select';
 import { ignoreSwitchFiles } from './elements/ignore/switch';
 
 const dev = process.env.ROLLUP_WATCH;
-
+const input = ['src/boilerplate-card.ts']
 const serveopts = {
-  contentBase: ['./dist'],
+  contentBase: ['./release'],
   host: '0.0.0.0',
   port: 5000,
   allowCrossOrigin: true,
@@ -29,19 +34,21 @@ const plugins = [
   json(),
   babel({
     exclude: 'node_modules/**',
+    extensions: [".js", ".svg"]
   }),
   dev && serve(serveopts),
   !dev && terser(),
   ignore({
     files: [...ignoreTextfieldFiles, ...ignoreSelectFiles, ...ignoreSwitchFiles].map((file) => require.resolve(file)),
   }),
+  postcss({}),
 ];
 
 export default [
   {
-    input: 'src/boilerplate-card.ts',
+    input: [...input],
     output: {
-      dir: 'dist',
+      dir: 'release',
       format: 'es',
     },
     plugins: [...plugins],
